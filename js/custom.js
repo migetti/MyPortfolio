e28925aa25f3083e7985669d14479ee90ec3594d
Mikael <migetti123@gmail.com>
2018-03-11 15:38:27 +0200

    Changes 2pac

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
   var options = [{
     selector: '#staggered-test',
-    offset: 300,
+    offset: 100,
     callback: function(el) {
       Materialize.showStaggeredList($(el));
-    }
-  }, {
-    selector: '#image-test',
-    offset: 300,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
     }
   }];
   
   Materialize.scrollFire(options);
 });
+
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
